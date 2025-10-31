8b3ca8445163407a8af14ec79ec7cf715bff5427
chewang07 <wangchen9617@gmail.com>
2025-10-31 23:23:33 +0800

    Add `venue` attribute to FXSpot and refactor symbol handling
    Introduced `venue` attribute in the FXSpot model to enhance granularity and enable `Venue.GLOBAL` assignment. Updated related logic and symbol handling in the Databento extractor to reference `asset` properties, ensuring consistency and improved data mapping.

--- src/datacore/models/assets/fx.py
+++ src/datacore/models/assets/fx.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+from datacore.models.mktdata.venue import Venue
 from datacore.models.assets.asset_type import AssetType
 from datacore.models.mktdata.base import BaseMarketData
 from datacore.models.assets.base import BaseAsset, TradingHours
@@ -7,6 +8,7 @@ from datacore.models.assets.base import BaseAsset, TradingHours
 
 class FXSpot(BaseAsset):
     hours: TradingHours
+    venue: Venue
     symbol: Optional[str] = None
     description: str = ""
     asset_type: AssetType = AssetType.FX
